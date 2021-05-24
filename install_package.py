9c73b8e253c7d16605a3c391356d5697a64941f0
Giovanni Lopez <giovannilopez9808>
2021-05-24 01:15:29 -0500

    Add install python and ubuntu package

---
+++ install_package.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+file = "package_list.csv"
+packages = np.loadtxt(file,
+                      usecols=0,
+                      delimiter=",",
+                      dtype=str)
+for package in packages:
+    package = package.split("/")[0]
+    print("\n\n")
+    print("--"*20)
+    print("Instalando paquete {}".format(package))
+    os.system("apt-get install {} -y".format(package))
+    print("--"*20)
+    print("\n\n")
---
+++ install_package_python.sh
@@ -0,0 +1,1 @@
+pip3 install -r package_python3.csv
