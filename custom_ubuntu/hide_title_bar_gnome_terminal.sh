ff26c2fed98bbe29f3210d65a6fa9e601ccf2490
Giovanni <giovannilopez9808@gmail.com>
2021-05-28 00:39:26 -0500

    Hide auto hide bar and menu on gnome-terminal, add list of extension in gnome

---
+++ custom_ubuntu/hide_title_bar_gnome_terminal.sh
@@ -0,0 +1,4 @@
+sudo apt install gnome-shell-extension-pixelsaver -y
+gnome-extensions enable pixel-saver@deadalnix.me
+gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
