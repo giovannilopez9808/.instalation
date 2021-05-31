338693f31ed5398a3b40f64968da8e778503302f
Giovanni <giovannilopez9808@gmail.com>
2021-05-31 02:51:55 -0500

    Delete useless line

--- Custom_Ubuntu/gnome_extensions/install_gnome_extension.sh
+++ Custom_Ubuntu/gnome_extensions/install_gnome_extension.sh
@@ -1,6 +1,5 @@
 sudo apt install gnome-tweaks gnome-shell-extensions chrome-gnome-shell -y
 echo "\n\n---------------------------------------------------------"
 echo "Press alt+f2 and enter the command 'r' to restart gnome 
-then press enter"
+	then press enter"
 echo "---------------------------------------------------------\n\n"
-read enter
