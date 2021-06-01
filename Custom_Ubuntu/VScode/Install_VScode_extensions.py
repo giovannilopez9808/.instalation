f7b1a52a255b55798544cd30e98fd322405d2187
Giovanni <giovannilopez9808@gmail.com>
2021-05-31 21:39:48 -0500

    Update install vscode extensions

--- Custom_Ubuntu/VScode/Install_VScode_extensions.py
+++ Custom_Ubuntu/VScode/Install_VScode_extensions.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
 extensions_names = np.loadtxt("vscode_extensions.csv",
-                              dtype=str)
+                    dtype=str)
 for extension in extensions_names:
+    text="Instalando extension de VScode {}".format(extension)
+    len_text=len(text)
     print("\n\n")
-    print("--"*20)
-    print("Instalando extension de VScode {}".format(extension))
-    print("--"*20)
+    print("-"*len_text)
+    print(text)
+    print("-"*len_text)
     print("\n\n")
     os.system("code --install-extension {}".format(extension))
