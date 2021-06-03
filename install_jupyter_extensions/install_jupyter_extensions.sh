8b562c91c2a991b280959c61523690bf6f8f166f
Giovanni <giovannilopez9808@gmail.com>
2021-06-03 03:35:36 -0500

    Install jupyter extensions

---
+++ install_jupyter_extensions/install_jupyter_extensions.sh
@@ -0,0 +1,6 @@
+pip3 install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+echo "----------------------------------------------------------------------------"
+echo "For more information plese visit"
+echo "https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html"
+echo "----------------------------------------------------------------------------"
