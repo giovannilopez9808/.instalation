03a3323ebcf0af18b0e014c0ddf1ebe8b2b3d2d0
Giovanni <giovannilopez9808@gmail.com>
2021-06-22 14:39:19 -0500

    Add Dracula Theme for GTK

---
+++ Custom_Ubuntu/Dracula_theme/GTK/install.sh
@@ -0,0 +1,10 @@
+wget https://github.com/dracula/gtk/archive/master.zip
+unzip master.zip
+rm master.zip
+sudo mv gtk-master /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+unzip Dracula.zip
+rm -rf Dracula
+sudo mv Dracula /usr/share/icons/
