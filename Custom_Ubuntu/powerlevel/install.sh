0d0ee80c33bb1b426dbda8fc011fdb87e10bdfd1
Giovanni <giovannilopez9808@gmail.com>
2021-06-26 23:53:08 -0500

    Fix bug on installing font

--- Custom_Ubuntu/powerlevel/install.sh
+++ Custom_Ubuntu/powerlevel/install.sh
@@ -1,3 +1,3 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-open MesloLGS_NF_Regular.ttf
+sudo cp MesloLGS_NF_Regular.ttf /usr/share/fonts
