21bdf61c0f7e48e007c20146716e47c7cad905ad
Giovanni <giovannilopez9808@gmail.com>
2021-07-11 03:26:41 -0500

    Use custom rofi web search

--- Custom_Ubuntu/rofi/install.sh
+++ Custom_Ubuntu/rofi/install.sh
@@ -5,6 +5,4 @@ echo "Instalando Rofi Web Search"
 echo
 echo "-----------------------------"
 echo
-git clone https://github.com/pdonadeo/rofi-web-search
-sudo mv rofi-web-search/rofi-web-search.py /bin/
-rm -rf rofi-web-search
+sudo cp rofi-web-search.py /bin/
