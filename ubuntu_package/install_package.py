84f00a63fdfca55f405ea718e5fa45e24bfa7ee4
Giovanni <giovannilopez9808@gmail.com>
2021-07-20 16:06:42 -0500

    Change print format

--- ubuntu_package/install_package.py
+++ ubuntu_package/install_package.py
@@ -7,13 +7,6 @@ packages = packages.split("\n")
 packages = packages[:-1]
 len_packages = len(packages)
 for number, package in enumerate(packages):
-    text = "Instalando paquete {}".format(package)
-    len_text = len(text)
-    print("\n\n")
-    print("-"*len_text)
+    text = "\t✅ Paquete {} instalado".format(package)
+    os.system("sudo apt-get -qq install {} -y".format(pacage))
     print(text)
-    print("Paquete numero {} de {}".format(number+1,
-                                           len_packages))
-    print("-"*len_text)
-    print("\n\n")
-    os.system("sudo apt-get install {} -y".format(package))
