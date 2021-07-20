24e4f54bb612ff4a1093e852ffea5dfa65e9db3a
Giovanni <giovannilopez9808@gmail.com>
2021-07-20 17:51:14 -0500

    Change names

--- Custom_Ubuntu/vscode/install.py
+++ Custom_Ubuntu/vscode/install.py
@@ -1,15 +1,10 @@
 import os
-extensions = open("vscode_extensions.csv",
+extensions = open("extensions.csv",
                   "r")
 extensions = extensions.read()
 extensions = extensions.split("\n")
 extensions = extensions[:-1]
 for extension in extensions:
-    text = "Instalando extension de VScode {}".format(extension)
-    len_text = len(text)
-    print("\n\n")
-    print("-"*len_text)
+    text = "\tExtension {} de vscode instalada ✅".format(extension)
     print(text)
-    print("-"*len_text)
-    print("\n\n")
     os.system("code --install-extension {}".format(extension))
