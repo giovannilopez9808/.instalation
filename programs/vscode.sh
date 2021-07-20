89c17584e266b60aec3243153c02576bad0efbbb
Giovanni <giovannilopez9808@gmail.com>
2021-07-20 17:13:59 -0500

    Silent vscode

--- programs/vscode.sh
+++ programs/vscode.sh
@@ -1,4 +1,4 @@
-sudo apt install software-properties-common apt-transport-https wget
+sudo apt-get -qq install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+sudo apt-get -qq install code
