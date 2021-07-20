2a1c51bfebb79fdbfd39232b84a2dfc7f4aee5ff
Giovanni <giovannilopez9808@gmail.com>
2021-07-20 16:14:26 -0500

    Make silent install

---
+++ tiling_window_manager/install.sh
@@ -0,0 +1,6 @@
+sudo apt-get -qq install node-typescript make git -y
+git clone https://github.com/pop-os/shell -qq
+cd shell
+make local-install
+cd ..
+rm -rf shell
--- tiling_window_manager/install_pop_shell.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install node-typescript make git -y
-git clone https://github.com/pop-os/shell
-cd shell
-make local-install
-cd ..
-rm -rf shell
