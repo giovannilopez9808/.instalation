4b60238703471208c63af0706c55e1cba639dbd2
Giovanni <giovannilopez9808@gmail.com>
2021-07-21 00:49:17 -0500

    Add emoji alert

--- jupyter_extensions/install.sh
+++ jupyter_extensions/install.sh
@@ -1,5 +1,4 @@
+#!/usr/bin/bash
 jupyter contrib nbextension install --user
-echo "----------------------------------------------------------------------------"
-echo "For more information plese visit"
+echo "❗For more information plese visit ❗"
 echo "https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html"
-echo "----------------------------------------------------------------------------"
