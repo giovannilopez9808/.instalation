4dda4c3b116de66608a3e0b44d1e73462369d58f
Giovanni <giovannilopez9808@gmail.com>
2021-07-21 02:06:51 -0500

    Add readme and bash format

---
+++ Custom_Ubuntu/powerlevel/README.md
@@ -0,0 +1,7 @@
+# Powerlevel
+
+Instalacion de powerlevel en la terminal
+
+#### Configuracion de powerlevel
+
+![powerlevel](https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/configuration-wizard.gif)
--- Custom_Ubuntu/powerlevel/install.sh
+++ Custom_Ubuntu/powerlevel/install.sh
@@ -1,4 +1,7 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#!/usr/bin/bash
+git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 sudo cp "MesloLGS NF Regular.ttf" /usr/share/fonts
 sudo fc-cache -f -v
+echo "✅ Se ha instalado powerlevel"
+echo "❗ Aplicar el comando p10k configure"
