969b53e2eea053ea13349d1a68f4b7096e259df1
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-10-14 12:36:55 -0500

    Add conan and delete flask

--- ubuntu_package/install.py
+++ ubuntu_package/install.py
@@ -8,6 +8,8 @@ packages = packages[:-1]
 len_packages = len(packages)
 for number, package in enumerate(packages):
     if package != "":
+        text_warm = "Instalando paquete {}".format(package)
+        print(text_warm)
         text = "✅ Paquete {} instalado".format(package)
-        os.system("sudo apt-get -qq install {} -y".format(package))
+        os.system("sudo apt-get install {} -y".format(package))
         print(text)
