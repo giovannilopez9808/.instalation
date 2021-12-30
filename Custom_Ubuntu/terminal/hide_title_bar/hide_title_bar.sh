cfeb74145874840c5866638425dfd623347c1ba7
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-12-30 13:24:39 -0600

    Delete pixel saver

--- Custom_Ubuntu/terminal/hide_title_bar/hide_title_bar.sh
+++ Custom_Ubuntu/terminal/hide_title_bar/hide_title_bar.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
-sudo apt-get -qq install gnome-shell-extension-pixelsaver
-gnome-extensions enable pixel-saver@deadalnix.me
 gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-echo "✅ Pixel saver se ha instalado"
 echo "❗ Press alt+f2 the r to restart gnome"
