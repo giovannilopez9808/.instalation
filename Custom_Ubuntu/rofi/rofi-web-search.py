4ef32c2d72f909ce438f254471b651242a421828
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-03-28 10:04:10 -0600

    Use firefox

--- Custom_Ubuntu/rofi/rofi-web-search.py
+++ Custom_Ubuntu/rofi/rofi-web-search.py
@@ -18,7 +18,7 @@ import html
 #####                      C O N F I G U R A T I O N                      ######
 ################################################################################
 SEARCH_ENGINE = 'google'            # or 'duckduckgo'
-BROWSER = 'chrome'                  # or 'firefox', 'chromium', 'brave', 'lynx'
+BROWSER = 'firefox'                  # or 'firefox', 'chromium', 'brave', 'lynx'
 TERMINAL = ['gnome-terminal', '--']  # or ['st', '-e'] or something like that
 ################################################################################
 
