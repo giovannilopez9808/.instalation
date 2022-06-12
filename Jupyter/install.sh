53d25a6a055c0bab51ff3e4f3b322b0632db393a
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-06-11 21:21:33 -0500

    Change name

---
+++ Jupyter/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+jupyter contrib nbextension install --user
+echo "❗For more information plese visit ❗"
+echo "https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/install.html"
