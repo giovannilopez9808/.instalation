696441ccc2f9e194f4c1b0b7b73fc67f75fe0649
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-06-11 21:29:30 -0500

    Delete useless line

--- Custom_Ubuntu/oh_my_zsh/install.sh
+++ Custom_Ubuntu/oh_my_zsh/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 sudo apt-get -qq install zsh git-core fonts-powerline -y
-wget -q https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 echo "✅ ZSH se ha instalado"
