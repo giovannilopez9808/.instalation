c6c927cfc5e925a57bd10d63598037027d61c6ec
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-06-11 20:45:53 -0500

    Change name

--- cargo_package/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install exa -q
-echo "✅ Se ha instalado exa"
-cargo install texlab -q
-echo "✅ Se ha instalado texlab"
